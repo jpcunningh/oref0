920c7d36d9e805f8b98f659a5ac266e188afc9a1
Jeremy Cunningham <jpcunningh@gmail.com>
2026-07-18 21:14:39 -0500

    go back to bt-pan and make it python3 compatible

--- bin/oref0-online.sh
+++ bin/oref0-online.sh
@@ -193,9 +193,12 @@ function bt_connect {
                 || ! jq -e .bt_offline < preferences.json > /dev/null \
                 || ! ifconfig | egrep -q "bnep0" >/dev/null; then
                 echo "Attempting to connect to bt $MAC..."
-                sudo bt-network --connect $MAC nap &
-                sleep 1
+                sudo bt-pan -i $hci_address client $MAC -d
+              for i in {1..3}
+              do
+                sudo bt-pan -i $hci_address client $MAC
                 sudo dhclient bnep0
+              done
                 if ! has_ip bnep0; then
                   bt_bnep0_cycle
                 fi
@@ -229,7 +232,10 @@ function bt_connect {
 function bt_disconnect {
     echo "Disconnecting BT $MAC"
     ifdown bnep0
-    pkill bt-network
+    # loop over as many MACs as are provided as arguments
+    for MAC; do
+        sudo bt-pan -i $hci_address client $MAC -d
+    done
 }
 
 function wifi_dhcp_renew {
